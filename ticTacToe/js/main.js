83d0aa86c6ebac48673707fd7cf023bce61c296e
sarahyf <alfadlysarah@gmail.com>
2018-10-12 11:48:36 +0300

    created game design and logic

---
+++ ticTacToe/js/main.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    var ticTacToe = {
+        1: { result: [{ id: "cell1", xo: "" }, { id: "cell2", xo: "" }, { id: "cell3", xo: "" }], matching: "" },
+        2: { result: [{ id: "cell4", xo: "" }, { id: "cell5", xo: "" }, { id: "cell6", xo: "" }], matching: "" },
+        3: { result: [{ id: "cell7", xo: "" }, { id: "cell8", xo: "" }, { id: "cell9", xo: "" }], matching: "" },
+        4: { result: [{ id: "cell1", xo: "" }, { id: "cell4", xo: "" }, { id: "cell7", xo: "" }], matching: "" },
+        5: { result: [{ id: "cell2", xo: "" }, { id: "cell5", xo: "" }, { id: "cell8", xo: "" }], matching: "" },
+        6: { result: [{ id: "cell3", xo: "" }, { id: "cell6", xo: "" }, { id: "cell9", xo: "" }], matching: "" },
+        7: { result: [{ id: "cell1", xo: "" }, { id: "cell5", xo: "" }, { id: "cell9", xo: "" }], matching: "" },
+        8: { result: [{ id: "cell3", xo: "" }, { id: "cell5", xo: "" }, { id: "cell7", xo: "" }], matching: "" }
+    };
+
+    var temp = "x";
+
+    $(document).click(function (event) {
+        var $id = event.target.id;
+
+        loop1: for (var keys in ticTacToe) {
+            loop2: for (var indexes in ticTacToe[keys].result) {
+                if (ticTacToe[keys].result[indexes].id === $id) {
+                    console.log($id);
+                    if ($("#" + $id).text() === "" && temp === "x") {
+                        temp = "o";
+                        if (ticTacToe[keys].result[indexes].xo === "") {
+                            ticTacToe[keys].result[indexes].xo = "O";
+                            ticTacToe[keys].matching += "O";
+                            $("#" + $id).text("O");
+                            console.log(ticTacToe[keys].matching);
+                        }
+                    } else if ($("#" + $id).text() === "" && temp === "o") {
+                        temp = "x";
+                        if (ticTacToe[keys].result[indexes].xo === "") {
+                            ticTacToe[keys].result[indexes].xo = "X";
+                            ticTacToe[keys].matching += "X";
+                            $("#" + $id).text("X");
+                            console.log(ticTacToe[keys].matching);
+                        }
+                    }
+                    if (ticTacToe[keys].matching === "XXX" || ticTacToe[keys].matching === "OOO") {
+                        $(document).off("click");
+                        console.log("STOP");
+                        break loop1;
+                    }
+                }
+            }
+        }
+    });
+});
